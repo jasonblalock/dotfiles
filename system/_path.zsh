b8473e426c00664adaf0a93804361b9d9ccbbc6a
jasonblalock <jasonblalock@users.noreply.github.com>
2018-04-14 11:37:56 -0400

    Update node and add local bin

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,6 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+if [ -d "$HOME/bin" ]
+then
+  export PATH=$PATH:$HOME/bin
+fi
